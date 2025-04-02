6fea1004e18ccb59baa5ccc09798defab4d435c9
Курзин Андрей <kyrzin>
2025-04-02 12:31:17 +0700

    27 lessons

---
+++ pages/posts/first-post.js
@@ -0,0 +1,12 @@
+import Head from 'next/head';
+import Layout from "../../components/Layout";
+
+export default function FirstPost() {
+    return (
+        <Layout>
+            <Head>
+                <title>First Post</title>
+            </Head>
+        </Layout>
+    )
+}
